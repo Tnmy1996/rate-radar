7a5f44a245525add12ea49495977af9e77941118
Tanmay Bindal <10maybinda@gmail.com>
2024-05-28 23:18:45 +0530

    refactor: Improve currency icon error handling and display

--- src/components/currency-icon.tsx
+++ src/components/currency-icon.tsx
@@ -1,12 +1,27 @@
+'use client';
+
+import { Currency } from 'lucide-react';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const CurrencyIcon = ({ coin }: { coin: string }) => {
+    const [isError, setIsError] = useState(false);
+
+    const onError = () => {
+        setIsError(true);
+    };
+
+    if (isError) {
+        return <Currency width={24} height={24} />;
+    }
+
     return (
         <Image
             src={`/cryptocurrency-icons/svg/color/${coin.toLowerCase()}.svg`}
             alt={coin}
             width={24}
             height={24}
+            onError={onError}
         />
     );
 };
