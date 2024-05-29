a76bf69f451843be3fc175f57bd8f403dec80893
Tanmay Bindal <10maybinda@gmail.com>
2024-05-29 10:43:43 +0530

    refactor: Update header component to use flex-wrap for better responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -41,7 +41,7 @@ export const Header = () => {
                                         {t('subheading')}
                                     </p>
                                 </div>
-                                <div className='flex items-center space-x-4'>
+                                <div className='flex flex-wrap items-center gap-4'>
                                     <CurrencyDropDown />
                                     <LanguageDropdown />
                                 </div>
